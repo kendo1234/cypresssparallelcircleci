69ee28cb13cd6f2b4623ec58953598318da87556
kendo1234 <kendomustdie@googlemail.com>
2019-02-21 05:14:30 -0500

    actual commit this time, new CI yaml

--- cypress/integration/Parallel/sample1.spec.js
+++ cypress/integration/Parallel/sample1.spec.js
@@ -2,6 +2,5 @@ describe('Cypress parallel run example - 1', () => {
   it('should display the title', () => {
     cy.visit(`https://mherman.org`);
     cy.get('a').contains('Michael Herman');
-    //comment
   });
 });
